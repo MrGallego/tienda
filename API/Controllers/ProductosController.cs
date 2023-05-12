42c910abfa1f6e36add2f6be186ba974052e17d3
MrGallego <44037673+MrGallego@users.noreply.github.com>
2023-05-12 02:10:56 -0500

    add endpoints

--- API/Controllers/ProductosController.cs
+++ API/Controllers/ProductosController.cs
@@ -33,5 +33,51 @@ namespace API.Controllers
             return Ok(producto);
         }
 
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Producto>> Create(Producto producto)
+        {
+            _unitOfWork.Productos.Add(producto);
+            _unitOfWork.Save();
+            if (producto == null) {
+                return BadRequest();
+            }
+
+            return CreatedAtAction(nameof(Create), new { id = producto.Id }, producto);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Producto>> Put(int id, [FromBody] Producto producto)
+        {
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Productos.Update(producto);
+            _unitOfWork.Save();
+
+            return producto;
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var producto = await _unitOfWork.Productos.GetByIdAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Productos.Remove(producto);
+            _unitOfWork.Save();
+
+            return NoContent();
+        }
+
     }
 }
